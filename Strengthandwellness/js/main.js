2dda99a24dd349b09d6df36305496db0567b7c38
Dalii90 <DaliiSRB@gmail.com>
2016-10-09 21:25:45 +0200

    Add files via upload

---
+++ Strengthandwellness/js/main.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('#comment').trigger('autoresize');
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
