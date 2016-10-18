202552f194ece648006d6e618a37a14a7a5ade3b
Dalii90 <DaliiSRB@gmail.com>
2016-10-18 22:16:36 +0200

    put cuba pictures

--- Strengthandwellness/js/main.js
+++ Strengthandwellness/js/main.js
@@ -5,15 +5,6 @@
     $('.parallax').parallax();
     $('.carousel.carousel-slider').carousel({full_width: true});
     $('#comment').trigger('autoresize');
-    $('.SW-collage').collagePlus(
-    {
-        // change this to adjust the height of the rows
-        'targetHeight' : 100,
-        // change this to try different effects
-        // valid effets = effect-1 to effect-6
-        'effect' : "effect-4"
-    }
-);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
